da815a47f8bf64d96673f503d71f2a09da9470ff
VijayWhat <vijayrv042@gmail.com>
2025-03-11 23:56:59 +0530

    Java project folder structure added

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class JsoupExample {
+    public static void main(String[] args) {
+        try {
+            Document doc = Jsoup.connect("https://example.com").get();
+            System.out.println(doc.title());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
